0d8ad0cdc4b4862144c2e5f12e9a59ecbbd616eb
Francesco Urbani <francescourbanidue@gmail.com>
2020-08-05 13:50:04 +0200

    updated version.

--- matching_network/__init__.py
+++ matching_network/__init__.py
@@ -11,7 +11,7 @@
 
 import quantiphy
 
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 
 
 PI = 3.141592653589793
--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@ from setuptools import setup
 
 setup(
     name='matching_network',
-    version='0.0.1',
+    version='0.0.2',
     author='Francesco Urbani',
     author_email='francescourbanidue@gmail.com',
     packages=['matching_network'],
     scripts=[],
-    url='http://pypi.python.org/pypi/matching_network/',
+    url='https://github.com/urbanij/matching-network',
     license='LICENSE.txt',
     description='Design matching network (L-sections)',
-    long_description=open('README.md').read(),
+    long_description=open('README.rst').read(),
     install_requires=[
         "quantiphy==2.10.0",
         "six==1.14.0",
