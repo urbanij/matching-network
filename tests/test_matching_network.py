7c7eb2ab551109cbf71963dad07a2acbba99e69c
Francesco Urbani <francescourbanidue@gmail.com>
2021-01-30 19:44:35 +0100

    moved code into multiple files.

--- tests/test_matching_network.py
+++ tests/test_matching_network.py
@@ -1,5 +1,11 @@
 from matching_network import __version__
+import toml
+
+def _read_version_from_toml_file():
+    with open("pyproject.toml", "r") as f:
+        content = f.read()
+    return toml.loads(content)['tool']['poetry']['version']
 
 
 def test_version():
-    assert __version__ == '0.0.5'
+    assert __version__ == _read_version_from_toml_file()
