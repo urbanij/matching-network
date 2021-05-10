a21010355e6548f73ccb534a666b47321209b4ff
Francesco Urbani <francescourbanidue@gmail.com>
2021-05-10 10:47:48 +0200

    updated version.

--- matching_network/__init__.py
+++ matching_network/__init__.py
@@ -8,7 +8,7 @@
 """Matching network module"""
 
 __title__ = 'matching_network'
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 __author__ = u"Francesco Urbani"
 
 from .notmain import L_section_matching
