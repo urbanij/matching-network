e26f2c206f811658c6ecf442910b000383cd7695
Francesco Urbani <francescourbanidue@gmail.com>
2021-06-08 14:40:53 +0200

    added possibility to pass by conductance in CLI mode

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -6,3 +6,25 @@ Changelog
 ------------------
 
 * First release on PyPI.
+
+
+0.1.6 (2021-06-08)
+------------------
+
+* Added possibility to pass by conductance, in mS, when called from CLI.
+  E.g.:
+  $ matching_network --from "24.3+8.3j mS"  --to 1.1+9.3j
+
+  From (36.85280111620006-12.587582274257635j) Ω to (1.1+9.3j) Ω
+
+  normalized starting impedance = (36.85280111620006-12.587582274257635j)Ω / (1.1+9.3j)Ω = -0.87259-4.0659j
+
+    #solutions: 4
+
+    shunt-series
+        Shunt Inductor:
+        X = 6.4545 Ω ⇔ B = -154.93 mS
+        Series Inductor:
+        X = 2.6624 Ω ⇔ B = -375.6 mS
+    [...]
+    
--- matching_network/__main__.py
+++ matching_network/__main__.py
@@ -17,13 +17,41 @@
 import click
 import matching_network as mn
 
-
 @click.command()
 # @click.argument('keyword', required=False)
-@click.option('--from', '-f', 'from_', required=True, type=complex)
-@click.option('--to', '-t', 'to', required=True, type=complex)
+@click.option('--from', '-f', 'from_', required=True, type=str)
+@click.option('--to', '-t', 'to', required=True, type=str)
 @click.option('--frequency', '--freq', 'frequency', required=False, type=float)
 def cli(from_, to, frequency):
+    if ("mS") in from_:
+        try:
+            from_mS = complex(from_.split()[0])
+            from_ = 1/(from_mS * 1e-3)
+        except Exception as e:
+            print(e)
+    else: # assumed ohm if unspecified
+        try:
+            from_ = complex(from_)
+        except ValueError:
+            print("Error: Did you mean `mS`?")
+            return
+        
+
+    if ("mS") in to:
+        try:
+            to_mS = complex(to.split()[0])
+            to = 1/(to_mS * 1e-3)
+        except Exception as e:
+            print(e)
+    else: # assumed ohm if unspecified
+        try:
+            to = complex(to)
+        except ValueError:
+            print("Error: Did you mean `mS`?")
+            return
+        
+
+    
     print(
         mn.L_section_matching(
             input_impedance=from_, 
